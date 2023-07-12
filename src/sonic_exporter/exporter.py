b8d35b32152fa4b529daaf785418b38afa6267d5
kamalkrbh <kamal.kr.bh@gmail.com>
2023-07-12 14:07:41 +0200

    Replace None with 0 in output JSON from redis DB.

--- src/sonic_exporter/exporter.py
+++ src/sonic_exporter/exporter.py
@@ -2118,226 +2118,226 @@ class SONiCCollector(object):
                 self.sonic_db.COUNTERS_DB, "CRM:ACL_STATS:EGRESS:LAG"
             )
             self.crm_acl_stats_egress_lag_crm_stats_acl_group_used.add_metric(
-                [], out_put.get("crm_stats_acl_group_used")
+                [], out_put.get("crm_stats_acl_group_used",0)
             )
             self.crm_acl_stats_egress_lag_crm_stats_acl_table_used.add_metric(
-                [], out_put.get("crm_stats_acl_table_used")
+                [], out_put.get("crm_stats_acl_table_used",0)
             )
             self.crm_acl_stats_egress_lag_crm_stats_acl_group_available.add_metric(
-                [], out_put.get("crm_stats_acl_group_available")
+                [], out_put.get("crm_stats_acl_group_available",0)
             )
             self.crm_acl_stats_egress_lag_crm_stats_acl_table_available.add_metric(
-                [], out_put.get("crm_stats_acl_table_available")
+                [], out_put.get("crm_stats_acl_table_available",0)
             )
             out_put = self.getAllFromDB(
                 self.sonic_db.COUNTERS_DB, "CRM:ACL_STATS:EGRESS:PORT"
             )
             self.crm_acl_stats_egress_port_crm_stats_acl_group_used.add_metric(
-                [], out_put.get("crm_stats_acl_group_used")
+                [], out_put.get("crm_stats_acl_group_used",0)
             )
             self.crm_acl_stats_egress_port_crm_stats_acl_table_used.add_metric(
-                [], out_put.get("crm_stats_acl_table_used")
+                [], out_put.get("crm_stats_acl_table_used",0)
             )
             self.crm_acl_stats_egress_port_crm_stats_acl_group_available.add_metric(
-                [], out_put.get("crm_stats_acl_group_available")
+                [], out_put.get("crm_stats_acl_group_available",0)
             )
             self.crm_acl_stats_egress_port_crm_stats_acl_table_available.add_metric(
-                [], out_put.get("crm_stats_acl_table_available")
+                [], out_put.get("crm_stats_acl_table_available",0)
             )
             out_put = self.getAllFromDB(
                 self.sonic_db.COUNTERS_DB, "CRM:ACL_STATS:EGRESS:RIF"
             )
             self.crm_acl_stats_egress_rif_crm_stats_acl_group_used.add_metric(
-                [], out_put.get("crm_stats_acl_group_used")
+                [], out_put.get("crm_stats_acl_group_used",0)
             )
             self.crm_acl_stats_egress_rif_crm_stats_acl_table_used.add_metric(
-                [], out_put.get("crm_stats_acl_table_used")
+                [], out_put.get("crm_stats_acl_table_used",0)
             )
             self.crm_acl_stats_egress_rif_crm_stats_acl_group_available.add_metric(
-                [], out_put.get("crm_stats_acl_group_available")
+                [], out_put.get("crm_stats_acl_group_available",0)
             )
             self.crm_acl_stats_egress_rif_crm_stats_acl_table_available.add_metric(
-                [], out_put.get("crm_stats_acl_table_available")
+                [], out_put.get("crm_stats_acl_table_available",0)
             )
             out_put = self.getAllFromDB(
                 self.sonic_db.COUNTERS_DB, "CRM:ACL_STATS:EGRESS:SWITCH"
             )
             self.crm_acl_stats_egress_switch_crm_stats_acl_group_used.add_metric(
-                [], out_put.get("crm_stats_acl_group_used")
+                [], out_put.get("crm_stats_acl_group_used",0)
             )
             self.crm_acl_stats_egress_switch_crm_stats_acl_table_used.add_metric(
-                [], out_put.get("crm_stats_acl_table_used")
+                [], out_put.get("crm_stats_acl_table_used",0)
             )
             self.crm_acl_stats_egress_switch_crm_stats_acl_group_available.add_metric(
-                [], out_put.get("crm_stats_acl_group_available")
+                [], out_put.get("crm_stats_acl_group_available",0)
             )
             self.crm_acl_stats_egress_switch_crm_stats_acl_table_available.add_metric(
-                [], out_put.get("crm_stats_acl_table_available")
+                [], out_put.get("crm_stats_acl_table_available",0)
             )
             out_put = self.getAllFromDB(
                 self.sonic_db.COUNTERS_DB, "CRM:ACL_STATS:EGRESS:VLAN"
             )
             self.crm_acl_stats_egress_vlan_crm_stats_acl_group_used.add_metric(
-                [], out_put.get("crm_stats_acl_group_used")
+                [], out_put.get("crm_stats_acl_group_used",0)
             )
             self.crm_acl_stats_egress_vlan_crm_stats_acl_table_used.add_metric(
-                [], out_put.get("crm_stats_acl_table_used")
+                [], out_put.get("crm_stats_acl_table_used",0)
             )
             self.crm_acl_stats_egress_vlan_crm_stats_acl_group_available.add_metric(
-                [], out_put.get("crm_stats_acl_group_available")
+                [], out_put.get("crm_stats_acl_group_available",0)
             )
             self.crm_acl_stats_egress_vlan_crm_stats_acl_table_available.add_metric(
-                [], out_put.get("crm_stats_acl_table_available")
+                [], out_put.get("crm_stats_acl_table_available",0)
             )
 
             out_put = self.getAllFromDB(
                 self.sonic_db.COUNTERS_DB, "CRM:ACL_STATS:INGRESS:LAG"
             )
             self.crm_acl_stats_ingress_lag_crm_stats_acl_group_used.add_metric(
-                [], out_put.get("crm_stats_acl_group_used")
+                [], out_put.get("crm_stats_acl_group_used",0)
             )
             self.crm_acl_stats_ingress_lag_crm_stats_acl_table_used.add_metric(
-                [], out_put.get("crm_stats_acl_table_used")
+                [], out_put.get("crm_stats_acl_table_used",0)
             )
             self.crm_acl_stats_ingress_lag_crm_stats_acl_group_available.add_metric(
-                [], out_put.get("crm_stats_acl_group_available")
+                [], out_put.get("crm_stats_acl_group_available",0)
             )
             self.crm_acl_stats_ingress_lag_crm_stats_acl_table_available.add_metric(
-                [], out_put.get("crm_stats_acl_table_available")
+                [], out_put.get("crm_stats_acl_table_available",0)
             )
             out_put = self.getAllFromDB(
                 self.sonic_db.COUNTERS_DB, "CRM:ACL_STATS:INGRESS:PORT"
             )
             self.crm_acl_stats_ingress_port_crm_stats_acl_group_used.add_metric(
-                [], out_put.get("crm_stats_acl_group_used")
+                [], out_put.get("crm_stats_acl_group_used",0)
             )
             self.crm_acl_stats_ingress_port_crm_stats_acl_table_used.add_metric(
-                [], out_put.get("crm_stats_acl_table_used")
+                [], out_put.get("crm_stats_acl_table_used",0)
             )
             self.crm_acl_stats_ingress_port_crm_stats_acl_group_available.add_metric(
-                [], out_put.get("crm_stats_acl_group_available")
+                [], out_put.get("crm_stats_acl_group_available",0)
             )
             self.crm_acl_stats_ingress_port_crm_stats_acl_table_available.add_metric(
-                [], out_put.get("crm_stats_acl_table_available")
+                [], out_put.get("crm_stats_acl_table_available",0)
             )
             out_put = self.getAllFromDB(
                 self.sonic_db.COUNTERS_DB, "CRM:ACL_STATS:INGRESS:RIF"
             )
             self.crm_acl_stats_ingress_rif_crm_stats_acl_group_used.add_metric(
-                [], out_put.get("crm_stats_acl_group_used")
+                [], out_put.get("crm_stats_acl_group_used",0)
             )
             self.crm_acl_stats_ingress_rif_crm_stats_acl_table_used.add_metric(
-                [], out_put.get("crm_stats_acl_table_used")
+                [], out_put.get("crm_stats_acl_table_used",0)
             )
             self.crm_acl_stats_ingress_rif_crm_stats_acl_group_available.add_metric(
-                [], out_put.get("crm_stats_acl_group_available")
+                [], out_put.get("crm_stats_acl_group_available",0)
             )
             self.crm_acl_stats_ingress_rif_crm_stats_acl_table_available.add_metric(
-                [], out_put.get("crm_stats_acl_table_available")
+                [], out_put.get("crm_stats_acl_table_available",0)
             )
             out_put = self.getAllFromDB(
                 self.sonic_db.COUNTERS_DB, "CRM:ACL_STATS:INGRESS:SWITCH"
             )
             self.crm_acl_stats_ingress_switch_crm_stats_acl_group_used.add_metric(
-                [], out_put.get("crm_stats_acl_group_used")
+                [], out_put.get("crm_stats_acl_group_used",0)
             )
             self.crm_acl_stats_ingress_switch_crm_stats_acl_table_used.add_metric(
-                [], out_put.get("crm_stats_acl_table_used")
+                [], out_put.get("crm_stats_acl_table_used",0)
             )
             self.crm_acl_stats_ingress_switch_crm_stats_acl_group_available.add_metric(
-                [], out_put.get("crm_stats_acl_group_available")
+                [], out_put.get("crm_stats_acl_group_available",0)
             )
             self.crm_acl_stats_ingress_switch_crm_stats_acl_table_available.add_metric(
-                [], out_put.get("crm_stats_acl_table_available")
+                [], out_put.get("crm_stats_acl_table_available",0)
             )
             out_put = self.getAllFromDB(
                 self.sonic_db.COUNTERS_DB, "CRM:ACL_STATS:INGRESS:VLAN"
             )
             self.crm_acl_stats_ingress_vlan_crm_stats_acl_group_used.add_metric(
-                [], out_put.get("crm_stats_acl_group_used")
+                [], out_put.get("crm_stats_acl_group_used",0)
             )
             self.crm_acl_stats_ingress_vlan_crm_stats_acl_table_used.add_metric(
-                [], out_put.get("crm_stats_acl_table_used")
+                [], out_put.get("crm_stats_acl_table_used",0)
             )
             self.crm_acl_stats_ingress_vlan_crm_stats_acl_group_available.add_metric(
-                [], out_put.get("crm_stats_acl_group_available")
+                [], out_put.get("crm_stats_acl_group_available",0)
             )
             self.crm_acl_stats_ingress_vlan_crm_stats_acl_table_available.add_metric(
-                [], out_put.get("crm_stats_acl_table_available")
+                [], out_put.get("crm_stats_acl_table_available",0)
             )
 
             out_put = self.getAllFromDB(self.sonic_db.COUNTERS_DB, "CRM:STATS")
             self.crm_stats_dnat_entry_used.add_metric(
-                [], out_put.get("crm_stats_dnat_entry_used")
+                [], out_put.get("crm_stats_dnat_entry_used",0)
             )
             self.crm_stats_fdb_entry_used.add_metric(
-                [], out_put.get("crm_stats_fdb_entry_used")
+                [], out_put.get("crm_stats_fdb_entry_used",0)
             )
             self.crm_stats_ipmc_entry_used.add_metric(
-                [], out_put.get("crm_stats_ipmc_entry_used")
+                [], out_put.get("crm_stats_ipmc_entry_used",0)
             )
             self.crm_stats_ipv4_neighbor_used.add_metric(
-                [], out_put.get("crm_stats_ipv4_neighbor_used")
+                [], out_put.get("crm_stats_ipv4_neighbor_used",0)
             )
             self.crm_stats_ipv4_nexthop_used.add_metric(
-                [], out_put.get("crm_stats_ipv4_nexthop_used")
+                [], out_put.get("crm_stats_ipv4_nexthop_used",0)
             )
             self.crm_stats_ipv4_route_used.add_metric(
-                [], out_put.get("crm_stats_ipv4_route_used")
+                [], out_put.get("crm_stats_ipv4_route_used",0)
             )
             self.crm_stats_ipv6_neighbor_used.add_metric(
-                [], out_put.get("crm_stats_ipv6_neighbor_used")
+                [], out_put.get("crm_stats_ipv6_neighbor_used",0)
             )
             self.crm_stats_ipv6_nexthop_used.add_metric(
-                [], out_put.get("crm_stats_ipv6_nexthop_used")
+                [], out_put.get("crm_stats_ipv6_nexthop_used",0)
             )
             self.crm_stats_ipv6_route_used.add_metric(
-                [], out_put.get("crm_stats_ipv6_route_used")
+                [], out_put.get("crm_stats_ipv6_route_used",0)
             )
             self.crm_stats_nexthop_group_member_used.add_metric(
-                [], out_put.get("crm_stats_nexthop_group_member_used")
+                [], out_put.get("crm_stats_nexthop_group_member_used",0)
             )
             self.crm_stats_nexthop_group_used.add_metric(
-                [], out_put.get("crm_stats_nexthop_group_used")
+                [], out_put.get("crm_stats_nexthop_group_used",0)
             )
             self.crm_stats_snat_entry_used.add_metric(
-                [], out_put.get("crm_stats_snat_entry_used")
+                [], out_put.get("crm_stats_snat_entry_used",0)
             )
             self.crm_stats_dnat_entry_available.add_metric(
-                [], out_put.get("crm_stats_dnat_entry_available")
+                [], out_put.get("crm_stats_dnat_entry_available",0)
             )
             self.crm_stats_fdb_entry_available.add_metric(
-                [], out_put.get("crm_stats_fdb_entry_available")
+                [], out_put.get("crm_stats_fdb_entry_available",0)
             )
             self.crm_stats_ipmc_entry_available.add_metric(
-                [], out_put.get("crm_stats_ipmc_entry_available")
+                [], out_put.get("crm_stats_ipmc_entry_available",0)
             )
             self.crm_stats_ipv4_neighbor_available.add_metric(
-                [], out_put.get("crm_stats_ipv4_neighbor_available")
+                [], out_put.get("crm_stats_ipv4_neighbor_available",0)
             )
             self.crm_stats_ipv4_nexthop_available.add_metric(
-                [], out_put.get("crm_stats_ipv4_nexthop_available")
+                [], out_put.get("crm_stats_ipv4_nexthop_available",0)
             )
             self.crm_stats_ipv4_route_available.add_metric(
-                [], out_put.get("crm_stats_ipv4_route_available")
+                [], out_put.get("crm_stats_ipv4_route_available",0)
             )
             self.crm_stats_ipv6_neighbor_available.add_metric(
-                [], out_put.get("crm_stats_ipv6_neighbor_available")
+                [], out_put.get("crm_stats_ipv6_neighbor_available",0)
             )
             self.crm_stats_ipv6_nexthop_available.add_metric(
-                [], out_put.get("crm_stats_ipv6_nexthop_available")
+                [], out_put.get("crm_stats_ipv6_nexthop_available",0)
             )
             self.crm_stats_ipv6_route_available.add_metric(
-                [], out_put.get("crm_stats_ipv6_route_available")
+                [], out_put.get("crm_stats_ipv6_route_available",0)
             )
             self.crm_stats_nexthop_group_available.add_metric(
-                [], out_put.get("crm_stats_nexthop_group_available")
+                [], out_put.get("crm_stats_nexthop_group_available",0)
             )
             self.crm_stats_nexthop_group_member_available.add_metric(
-                [], out_put.get("crm_stats_nexthop_group_member_available")
+                [], out_put.get("crm_stats_nexthop_group_member_available",0)
             )
             self.crm_stats_snat_entry_available.add_metric(
-                [], out_put.get("crm_stats_snat_entry_available")
+                [], out_put.get("crm_stats_snat_entry_available",0)
             )
 
         except Exception as e:
